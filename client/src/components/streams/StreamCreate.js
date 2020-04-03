c5686c06784c824dd3190c006a5d7a9f56a8af9a
Artur Socha <artur.socha00@gmail.com>
2020-04-03 21:13:08 +0200

    redux-form - initialization generating form(stream-create)

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+  renderInput (formProps) {
+    return (
+      <div className='form__group'>
+        <label for={formProps.input.name} className='form__label'>{formProps.input.name}</label>
+        <input 
+          type='text' 
+          className='form__input' 
+          id={formProps.input.name} 
+          value={formProps.input.value} 
+          onChange={formProps.input.onChange}/>
+      </div>
+    );
+  }
+
+  render(){
+    return (
+      <form className='form'>
+        <Field name='title' component={ this.renderInput }/>
+        <Field name='description' component={ this.renderInput }/>
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
