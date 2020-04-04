8261fb37173965f2f7f841fb2d2362617a1673b1
Artur Socha <artur.socha00@gmail.com>
2020-04-04 19:29:06 +0200

    -stream-actions, -streamReducer, -fontAwesome including,

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,31 +1,68 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions/index';
 
 class StreamCreate extends React.Component {
-  renderInput (formProps) {
+  renderError = ({ touched, error }) => {
+    if(touched && error){
+      return(
+        <div className='form__error'>
+          { error }
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, meta }) => {
     return (
       <div className='form__group'>
-        <label for={formProps.input.name} className='form__label'>{formProps.input.name}</label>
+        <label className='form__label'>{input.name}</label>
         <input 
           type='text' 
           className='form__input' 
-          id={formProps.input.name} 
-          value={formProps.input.value} 
-          onChange={formProps.input.onChange}/>
+          {...input}
+          autoComplete='off'
+        />
+        <div className='form__message'>
+          {this.renderError(meta)}
+        </div>
       </div>
     );
   }
 
+  onSubmit = formValues =>{
+    this.props.createStream(formValues);
+  }
+
   render(){
     return (
-      <form className='form'>
+      <form onSubmit={ this.props.handleSubmit(this.onSubmit) } className='form'>
         <Field name='title' component={ this.renderInput }/>
         <Field name='description' component={ this.renderInput }/>
+        <button className='btn btn--primary'>Submit</button>
       </form>
     );
   }
 }
 
-export default reduxForm({
-  form: 'streamCreate'
+const validate = (formValues) => {
+  const errors = {}
+  if(!formValues.title){
+    errors.title = 'You must name your stream';
+  }
+  if(!formValues.description){
+    errors.description = 'You must add a description to your stream'
+  }
+
+  return errors;
+}
+
+const formWrapped = reduxForm({
+  form: 'streamCreate',
+  validate
 })(StreamCreate);
+
+export default connect(null, { 
+  createStream
+ } )(formWrapped);
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions/index';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons'
 
-const StreamList = () => {
-  return <div>StreamList</div>
+class StreamList extends React.Component {
+  componentDidMount(){
+    this.props.fetchStreams();
+  }
+
+  renderList() {
+    return this.props.streams.map(stream => {
+      return(
+        <div className='stream'>
+          <div className='stream__info'>
+            <div className='stream__info--title'></div>
+            <div className='stream__info--desc'></div>
+          </div>
+          <div className='stream__enter'>
+          <FontAwesomeIcon icon={faPlayCircle} className='stream__enter--icon'></FontAwesomeIcon>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render(){
+    console.log(this.props.streams)
+    return (
+      <div className='stream__list'>
+        { this.renderList() }
+      </div>
+    );
+  }
 }
 
-export default StreamList;
+const mapStateToProps = (state) =>{
+  return {
+    streams: Object.values(state.streams)
+  }
+}
+
+export default connect(mapStateToProps,
+  { fetchStreams }
+)(StreamList);
